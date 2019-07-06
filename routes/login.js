e18570e073b2f9b12597b81d335d075d9d1babe3
shin june-hyuk <47058441+shinjjune@users.noreply.github.com>
2019-07-06 22:16:21 +0900

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-
+/* GET home page. */
 router.post('/', function(req, res, next) {
-  const result={msg:''};
-  console.log("세션iD=",req.sessionID);
-  if(req.session.name === req.body.name){
-    req.session.loginState=true;
-    res.redirect('/');
-    
-  }else{
-    result.msg='다시로그인해주세요';
-    res.json(JSON.stringify(result));
-  }
- 
+ const result={msg:''};
+ console.log("세션ID(login.js)",req.sessionID);
+ /* console.log(req.session.email);
+ console.log(req.body.email); */
+ if(req.session.userid === req.body.userid){
+     req.session.loginState = true;
+     res.redirect("/");
+    //  result.msg=`${req.session.name}님 로그인되셨습니다.`;
+ }else{
+    res.json(JSON.stringify({msg:"다시 로그인해주세요~"}));
+ }
 
 });
+
 module.exports = router;
