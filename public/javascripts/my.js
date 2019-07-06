3beb5c2fb3399493653c672dbefa342fac686f34
shin june-hyuk <47058441+shinjjune@users.noreply.github.com>
2019-07-06 22:18:02 +0900

    Update my.js

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('#member_insert_btn').click(function(){
-        const id=$('#id').val();
+        const userid=$('#userid').val();
         const pw=$('#pw').val();
         const name=$('#name').val();
         const send_params={
-            id,
+            userid,
             pw,
             name
         };
-        $.post("member_insert",send_params,function(data,status){
+        $.post("/member_insert",send_params,function(data,status){
             alert(data+":"+status);
             const parsed_data=JSON.parse(data);
 
@@ -16,18 +16,14 @@ $(document).ready(function() {
         });
     });
     $('#login_btn').click(function () {
-        const id=$('#login_id').val();
-        const pw=$('#login_pw').val();
+        const userid=$('#login_userid').val();
         const send_params={
-            id,
-            pw
+            userid,
         };
         $.post("/login",send_params,function (data,status) {
             try {
                 alert(JSON.parse(data).msg);
-                $('#login_id').val()="";
-                $('#login_pw').val()="";
-
+                $('#login_userid').val()="";
             } catch (err) {
                 window.location.reload(true);
                 
@@ -43,4 +39,34 @@ $(document).ready(function() {
         });
     });
     
+  
+
+    $('#searchType_btn').click(function () {
+        const searchType=$('#searchType').val();
+        const insur_input=$('#insur_input').val();
+        const send_params={
+            searchType,
+            insur_input
+        };
+        $.post("/search_insur",send_params,function (data,status) {
+            //alert(data+":"+status);
+            const parsed_data=JSON.parse(data);
+            //console.log(parsed_data)
+            //console.log(parsed_data.msg)
+
+            if(parsed_data.msg=="조회결과가 없습니다")
+            {
+                $('#table_div').html(parsed_data.msg);
+            }else{
+                let printData='<table border=1>';
+                for(var key in parsed_data.msg) {
+                    printData += `<tr><td>${key}</td><td>${parsed_data.msg[key]}</td></tr>`;
+                }
+                printData += '</table>';
+                $('#table_div').html(printData);
+
+            }
+        });
+
+    });
 });
