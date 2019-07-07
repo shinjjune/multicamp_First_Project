ecb3352c10b80a32bcaed2f68c4ae716f770cc93
shin june-hyuk <47058441+shinjjune@users.noreply.github.com>
2019-07-07 20:52:13 +0900

    Update and rename Career.js to Enrollment.js

--- routes/Enrollment.js
+++ routes/Enrollment.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Career',{
+  res.render('Enrollment',{
     userid:req.session.userid,
   loginState:req.session.loginState});
 });
